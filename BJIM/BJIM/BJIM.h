e5c6cf0440901541eaf43a17e9d36ad6332c49a8
YangLei-bjhl <jxyl9010@gmail.com>
2015-05-08 22:47:48 +0800

    测试工程

--- BJIM/BJIM/BJIM.h
+++ BJIM/BJIM/BJIM.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char BJIMVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BJIM/PublicHeader.h>
 
+#define BJIM_VERSTION 1.0
 
