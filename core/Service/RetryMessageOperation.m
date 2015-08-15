e5d969ff37fedba8624e6885535b398e36624058
yanglei <jxyl9010@gmail.com>
2015-08-15 21:34:06 +0800

    msgid

--- core/Service/RetryMessageOperation.m
+++ core/Service/RetryMessageOperation.m
@@ -21,7 +21,7 @@
     
     Conversation *conversation = [self.imService getConversationUserOrGroupId:self.message.receiver userRole:self.message.receiverRole ownerId:self.message.sender ownerRole:self.message.senderRole chat_t:self.message.chat_t];
     
-    self.message.msgId = [NSString stringWithFormat:@"%011.3lf", [[self.imService.imStorage queryAllMessageMaxMsgId] doubleValue] + 0.001];
+    self.message.msgId = [NSString stringWithFormat:@"%015.3lf", [[self.imService.imStorage queryAllMessageMaxMsgId] doubleValue] + 0.001];
     
     conversation.lastMessageId = self.message.msgId;
     
