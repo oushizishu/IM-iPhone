c49038dbf96769fea99b7d97fd1593e390b1defa
yanglei <jxyl9010@gmail.com>
2015-10-26 12:05:11 +0800

    code review

---
+++ core/Vendor/BJIMUrlSchema.h
@@ -0,0 +1,16 @@
+//
+//  BJIMUrlSchema.h
+//  Pods
+//
+//  Created by 杨磊 on 15/10/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BJIMUrlSchema : NSObject
+
++ (instancetype)shareInstance;
+
+
+@end
---
+++ core/Vendor/BJIMUrlSchema.m
@@ -0,0 +1,13 @@
+//
+//  BJIMUrlSchema.m
+//  Pods
+//
+//  Created by 杨磊 on 15/10/26.
+//
+//
+
+#import "BJIMUrlSchema.h"
+
+@implementation BJIMUrlSchema
+
+@end
